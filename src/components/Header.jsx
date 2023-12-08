bc7c591260752e2850c351060905257e38b11a1c
sam-gardner <samgardner909@gmail.com>
2023-12-08 15:40:13 +0000

    Style header for mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,13 @@ import { Link } from 'gatsby';
 const Header = () => {
   return (
     <nav>
-      <h1>Great North Gardens</h1>
-      <div className='links'>
-        <Link to='/'>Home</Link>
-        <Link to='/services'>Services</Link>
-        <Link to='/contact'>Contact</Link>
+      <div>
+        <h1 className='logo'>Great North Gardens</h1>
+        <div className='links'>
+          <Link to='/'>Home</Link>
+          <Link to='/services'>Services</Link>
+          <Link to='/contact'>Contact</Link>
+        </div>
       </div>
     </nav>
   );
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -6,7 +6,9 @@ const Contact = () => {
     <div className="layout">
       <Header />
       <h1>Contact Details</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel eros ante. Phasellus tempor magna non posuere gravida. Aliquam tincidunt ipsum quis est tincidunt posuere. In vitae laoreet velit, sit amet pretium neque. Phasellus ut dignissim augue, non euismod ex. Sed mollis arcu sed efficitur facilisis. Suspendisse aliquet mi in finibus laoreet. Fusce augue ex, efficitur vitae volutpat quis, aliquet et tortor. Proin molestie aliquam velit vel condimentum. Aliquam luctus sem eu libero commodo convallis. Quisque sed bibendum dolor, ut mattis mauris. Phasellus eleifend lacinia iaculis. Donec at sem eget risus dapibus imperdiet a in arcu. Etiam vitae placerat mi. Etiam urna libero, malesuada eget risus eu, euismod posuere mauris.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel eros ante. Phasellus tempor 
+        magna non posuere gravida. Aliquam tincidunt ipsum quis est tincidunt posuere. In vitae laoreet velit, 
+        Donec at sem eget risus dapibus imperdiet a in arcu. Etiam vitae placerat mi. Etiam urna libero.</p>
     </div>
   );
 };
