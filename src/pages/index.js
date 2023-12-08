336ebb8f6556dd8dfceb1995de5e70fde2f80121
sam-gardner <samgardner909@gmail.com>
2023-12-08 13:05:59 +0000

    Test change to text

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
-import * as React from "react"
+import * as React from 'react';
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <h1>Welcome to Great Northern Gardens</h1>
+      <h2>Website coming soon</h2>
+    </>
+  );
 }
